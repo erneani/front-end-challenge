c09e5db108f125942f30c31cc614e5e7e9957cef
vinicius <vinicius@tootz.com.br>
2018-11-19 08:29:20 -0300

    Fix: Add the middle line to the previous price

--- src/components/large-label/LargeLabel.js
+++ src/components/large-label/LargeLabel.js
@@ -4,6 +4,6 @@ import './style.scss';
 
 export default class LargeLabel extends React.Component {
   render() {
-    return <label className="large-label"> { this.props.label } </label>
+    return <label className="large-label"> {this.props.label} <spam className="label-previous-price">{this.props.price}</spam></label>
   }
 }
--- src/components/price-container/PriceContainer.js
+++ src/components/price-container/PriceContainer.js
@@ -9,7 +9,7 @@ export default class PriceContainer extends React.Component {
   render() {
     return (
       <div className="price-container">
-        <LargeLabel label="de R$44.120,00" />
+        <LargeLabel label="de" price="R$44.120,00" />
         <BigPriceLabel label="por R$" price="40.987,00" />
         <WarningLabel warning="*Consulte as Condições Comerciais." />
       </div>

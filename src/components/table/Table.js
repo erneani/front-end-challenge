11289e0ca867713465bc774ab071bb134f3e2a8a
vinicius <vinicius@tootz.com.br>
2018-11-19 12:26:37 -0300

    Personalize the scrollbar style

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -26,6 +26,54 @@ export const Table = (props) => {
         <TableRow header="Diâmetro/curso (mm):" element="87.5/8.31" />
         <TableRow header="Cilindros/válvulas por cilíndro:" element="4/4" />
         <TableRow header="Torque máximo EEC - Nm (libra-pé):" element="340" />
+        <TableRow header="Cilindrada (cc):" element="1,999" /><TableRow header="Cilindros/válvulas por cilíndro:" element="4/4" />
+        <TableRow header="Diâmetro/curso (mm):" element="87.5/8.31" />
+        <TableRow header="Cilindrada (cc):" element="1,999" />
+        <TableRow header="Potência máxica EEC - Cv:" element="240" />
+        <TableRow header="em rev/min:" element="5,500" />
+        <TableRow header="Torque máximo EEC - Nm (libra-pé):" element="340" />
+        <TableRow header="em rev/min:" element="2,000-4,000" />
+        <TableRow header="Potência máxica EEC - Cv:" element="240" />
+        <TableRow header="Diâmetro/curso (mm):" element="87.5/8.31" />
+        <TableRow header="Diâmetro/curso (mm):" element="87.5/8.31" />
+        <TableRow header="Torque máximo EEC - Nm (libra-pé):" element="340" />
+        <TableRow header="Potência máxica EEC - Cv:" element="240" />
+        <TableRow header="Potência máxica EEC - Cv:" element="240" />
+        <TableRow header="Diâmetro/curso (mm):" element="87.5/8.31" />
+        <TableRow header="Cilindros/válvulas por cilíndro:" element="4/4" />
+        <TableRow header="Torque máximo EEC - Nm (libra-pé):" element="340" />
+        <TableRow header="Cilindrada (cc):" element="1,999" /><TableRow header="Cilindros/válvulas por cilíndro:" element="4/4" />
+        <TableRow header="Diâmetro/curso (mm):" element="87.5/8.31" />
+        <TableRow header="Cilindrada (cc):" element="1,999" />
+        <TableRow header="Potência máxica EEC - Cv:" element="240" />
+        <TableRow header="em rev/min:" element="5,500" />
+        <TableRow header="Torque máximo EEC - Nm (libra-pé):" element="340" />
+        <TableRow header="em rev/min:" element="2,000-4,000" />
+        <TableRow header="Potência máxica EEC - Cv:" element="240" />
+        <TableRow header="Diâmetro/curso (mm):" element="87.5/8.31" />
+        <TableRow header="Diâmetro/curso (mm):" element="87.5/8.31" />
+        <TableRow header="Torque máximo EEC - Nm (libra-pé):" element="340" />
+        <TableRow header="Potência máxica EEC - Cv:" element="240" />
+        <TableRow header="Potência máxica EEC - Cv:" element="240" />
+        <TableRow header="Diâmetro/curso (mm):" element="87.5/8.31" />
+        <TableRow header="Cilindros/válvulas por cilíndro:" element="4/4" />
+        <TableRow header="Torque máximo EEC - Nm (libra-pé):" element="340" />
+        <TableRow header="Cilindrada (cc):" element="1,999" /><TableRow header="Cilindros/válvulas por cilíndro:" element="4/4" />
+        <TableRow header="Diâmetro/curso (mm):" element="87.5/8.31" />
+        <TableRow header="Cilindrada (cc):" element="1,999" />
+        <TableRow header="Potência máxica EEC - Cv:" element="240" />
+        <TableRow header="em rev/min:" element="5,500" />
+        <TableRow header="Torque máximo EEC - Nm (libra-pé):" element="340" />
+        <TableRow header="em rev/min:" element="2,000-4,000" />
+        <TableRow header="Potência máxica EEC - Cv:" element="240" />
+        <TableRow header="Diâmetro/curso (mm):" element="87.5/8.31" />
+        <TableRow header="Diâmetro/curso (mm):" element="87.5/8.31" />
+        <TableRow header="Torque máximo EEC - Nm (libra-pé):" element="340" />
+        <TableRow header="Potência máxica EEC - Cv:" element="240" />
+        <TableRow header="Potência máxica EEC - Cv:" element="240" />
+        <TableRow header="Diâmetro/curso (mm):" element="87.5/8.31" />
+        <TableRow header="Cilindros/válvulas por cilíndro:" element="4/4" />
+        <TableRow header="Torque máximo EEC - Nm (libra-pé):" element="340" />
         <TableRow header="Cilindrada (cc):" element="1,999" />
       </tbody>
     </table>
